db50cfd1d5eed5dae1fff2b0aaf959f2d915742e
FabricioMoreno <fabricio.morenonaranjo@hotmail.com>
2023-05-09 21:02:31 -0500

    Close toDo design added

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -3,12 +3,13 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import { TextField } from "@mui/material";
-import Swal from "sweetalert2"
+import { TextField, IconButton } from "@mui/material";
+import Swal from "sweetalert2";
 
-import actions from "../actions"
-import {writeFile} from "../fileIO"
-import {GlobalContext} from "../contexts/GlobalContext"
+import actions from "../actions";
+import { writeFile } from "../fileIO";
+import { GlobalContext } from "../contexts/GlobalContext";
+import CloseIcon from "@mui/icons-material/Close";
 
 const style = {
   position: "absolute",
@@ -26,7 +27,6 @@ const singleToDoStyle = {
   width: "100%",
   background: "#6395a3",
   color: "#ffffff",
-  marginBottom: "15px",
   "&:hover": {
     backgroundColor: "#49727e",
   },
@@ -71,87 +71,107 @@ export default function ToDo({
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const {loadAlltFiles}= useContext(GlobalContext)!
+  const { loadAlltFiles } = useContext(GlobalContext)!;
 
   function capitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
   }
 
-  async function executeAction(action:string){
-    if(/^\s*$/.test(title)){
-      console.log("escriba un titulo")
+  async function executeAction(action: string) {
+    if (/^\s*$/.test(title)) {
+      console.log("escriba un titulo");
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'You must write a title!',
-      })
-      handleClose()
-      return
+        icon: "error",
+        title: "Oops...",
+        text: "You must write a title!",
+      });
+      handleClose();
+      return;
     }
 
-    if(/^\s*$/.test(content)){
-      console.log("escriba un titulo")
+    if (/^\s*$/.test(content)) {
+      console.log("escriba un titulo");
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'You must write a content!',
-      })
-      handleClose()
-      return
+        icon: "error",
+        title: "Oops...",
+        text: "You must write a content!",
+      });
+      handleClose();
+      return;
     }
 
-    if(action === actions.create){
-      console.log("here")
-      try{
-        console.log(title,content,"---")
-        await writeFile(title,content);
-        await loadAlltFiles()
-        handleClose()
+    if (action === actions.create) {
+      console.log("here");
+      try {
+        console.log(title, content, "---");
+        await writeFile(title, content);
+        await loadAlltFiles();
+        handleClose();
         Swal.fire({
-          icon: 'success',
-          title: 'Successfully',
-          text: 'ToDo created successfully!',
-        })
-
-      }catch(err){
-        console.error("Error in create a file")
+          icon: "success",
+          title: "Successfully",
+          text: "ToDo created successfully!",
+        });
+      } catch (err) {
+        console.error("Error in create a file");
       }
-      return
+      return;
     }
-    
-    if(action === actions.edit){
-      try{
-        console.log(title,content,"-editing--")
-        await writeFile(title,content);
-        await loadAlltFiles()
-        handleClose()
+
+    if (action === actions.edit) {
+      try {
+        console.log(title, content, "-editing--");
+        await writeFile(title, content);
+        await loadAlltFiles();
+        handleClose();
         Swal.fire({
-          icon: 'success',
-          title: 'Successfully',
-          text: 'ToDo edited successfully!',
-        })
-
-      }catch(err){
-        console.error("Error in editing a file")
+          icon: "success",
+          title: "Successfully",
+          text: "ToDo edited successfully!",
+        });
+      } catch (err) {
+        console.error("Error in editing a file");
       }
-      return
+      return;
     }
-
   }
 
   return (
     <div>
       {type === "edit" ? (
-        <Button onClick={handleOpen} sx={singleToDoStyle}>
-          {title.toUpperCase()}
-        </Button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: "15px",
+          }}
+        >
+          <Button onClick={handleOpen} sx={singleToDoStyle}>
+            {title.toUpperCase()}
+            <IconButton
+              sx={{
+                position: "absolute",
+                right: "0px",
+                "&:hover, & .Mui-focusVisible": {
+                  backgroundColor: "rgb(10, 89, 109)",
+                  color: "white",
+                },
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Button>
+        </div>
       ) : (
-        <button onClick={()=>{ 
-          setTitle("")
-          setContent("")
-          handleOpen()
-          }} 
-          style={buttonCreateToDoStyle}>
+        <button
+          onClick={() => {
+            setTitle("");
+            setContent("");
+            handleOpen();
+          }}
+          style={buttonCreateToDoStyle}
+        >
           Create toDo
         </button>
       )}
@@ -187,15 +207,20 @@ export default function ToDo({
               variant="contained"
               color="warning"
               sx={{ width: "35%" }}
-              onClick={()=>{
-                setTitle(tf),
-                setContent(tc)
-                handleClose()
+              onClick={() => {
+                setTitle(tf);
+                setContent(tc);
+                handleClose();
               }}
             >
               Discard
             </Button>
-            <Button variant="contained" color="success" sx={{ width: "35%" }} onClick={(e)=>executeAction(type)}>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ width: "35%" }}
+              onClick={(e) => executeAction(type)}
+            >
               {type.toUpperCase()}
             </Button>
           </div>
